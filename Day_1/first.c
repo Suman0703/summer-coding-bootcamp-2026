006316ad9361ad84daf1b1cbcaf6050447f652ca
Suman0703 <sumandevimehmi36@gmail.com>
2026-06-03 22:00:26 +0530

    Day 1: Print Hello World using for loop in C

---
+++ Day_1/first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf("Hello This is my First C program");
+
+    // return 0;
+
+    for (int i = 0; i <= 20; i++)
+    {
+        printf("Hello World! ");
+        printf("%d\n", i);
+    }
+
+    return 0;
+};
