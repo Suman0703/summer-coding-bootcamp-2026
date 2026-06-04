184aac1f9c4c4f6cf8c0dc83dfb6bc4954a8ab64
Suman0703 <sumandevimehmi36@gmail.com>
2026-06-04 19:52:45 +0530

    Day 2: Learned C data types and functions

---
+++ Day_2/Assignment/program1.c
@@ -0,0 +1,17 @@
+// Write a C program to Print your name, Date of Birth, and Mobile Number
+#include <stdio.h>
+
+int main() {
+
+    int age = 19;
+    char mobileNo[] = "+91-7347560452";
+    char DOB[] = "September 7, 2006";
+    char name[] = "Suman Devi";
+
+    printf("Name : %s\n", name);
+    printf("Age : %d\n", age);
+    printf("DOB : %s\n", DOB);
+    printf("Mobile No. : %s\n", mobileNo);
+
+    return 0;
+}
---
+++ Day_2/Assignment/program2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 123;
+    float y = 34.67f;
+    char ch = 'A';
+
+    printf("Integer = %d\n", x);
+    //float with 2 decimal places
+    printf("Float = %.2f\n", y);
+    printf("Character = %c\n", ch);
+
+    return 0;
+}
