d10efc1b7c743757035466787660fcd121a4f4f1
Suman0703 <sumandevimehmi36@gmail.com>
2026-06-07 19:46:25 +0530

    Add triangle area and string reverse programs

---
+++ Day_3/Assignment2/program3.c
@@ -0,0 +1,18 @@
+// C program to find area of an equilateral triangle
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float side, area;
+
+    printf("Enter side of the equilateral triangle: ");
+    scanf("%f", &side);
+
+    area = (sqrt(3) / 4) * (side * side);
+
+    printf("Area of equilateral triangle = %.2f sq. units", area);
+
+    return 0;
+}
