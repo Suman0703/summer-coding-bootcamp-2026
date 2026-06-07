80d06d1856cab78187a72a8a1e935ff606792572
Suman0703 <sumandevimehmi36@gmail.com>
2026-06-07 20:05:33 +0530

    add c program to calculate simple interest

--- Day_3/Assignment2/program10.c
+++ Day_3/Assignment2/program10.c
@@ -1,3 +1,24 @@
 /*Write a C program to input principle, time and rate (P, T, R) from user and find Simple Interest.
 How to calculate simple interest in C programming.*/
 
+#include <stdio.h>
+
+int main()
+{
+    float principal, rate, time, simpleInterest;
+
+    printf("Enter Principal Amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter Time (years): ");
+    scanf("%f", &time);
+
+    printf("Enter Rate of Interest: ");
+    scanf("%f", &rate);
+
+    simpleInterest = (principal * time * rate) / 100;
+
+    printf("Simple Interest = %.2f\n", simpleInterest);
+
+    return 0;
+}
