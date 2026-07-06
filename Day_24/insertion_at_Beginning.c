06332c6cd1ad340a38e1fcbc9b2d64bfbc8965f1
Suman0703 <sumandevimehmi36@gmail.com>
2026-07-06 20:25:06 +0530

    Add linked list insertion at beginning and end programs

---
+++ Day_24/insertion_at_Beginning.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int num;
+    struct Node *next;
+};
+
+struct Node *start = NULL;
+
+// Function Declarations
+void insertAtBeginning();
+void display();
+
+int main()
+{
+    int choice;
+
+    do
+    {
+        printf("1. Insert Node\n");
+        printf("2. Display List\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            insertAtBeginning();
+            break;
+
+        case 2:
+            display();
+            break;
+
+        case 3:
+            printf("Exiting...\n");
+            break;
+
+        default:
+            printf("Invalid Choice!\n");
+        }
+
+    } while (choice != 3);
+
+    return 0;
+}
+
+void insertAtBeginning()
+{
+    struct Node *newNode;
+
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    if (newNode == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+
+    printf("Enter data: ");
+    scanf("%d", &newNode->num);
+
+    newNode->next = start;
+    start = newNode;
+
+    printf("Node inserted successfully.\n");
+}
+
+void display()
+{
+    struct Node *temp;
+
+    if (start == NULL)
+    {
+        printf("Linked List is Empty.\n");
+        return;
+    }
+
+    temp = start;
+
+    printf("\nLinked List:\n");
+
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->num);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
